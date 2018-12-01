ccb7037f85dc74fa0423a4ead83d86d686220bd5
Raveendra Lingareddygari <36727733+Raveendra7@users.noreply.github.com>
2018-12-01 13:00:37 +0530

    Update RankedGradeBook.cs
    Commiting Override RankedGradeBook's GetLetterGrade changes.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,30 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            var result = 'F';
+
+            if (grades[threshold - 1] <= averageGrade)
+                result = 'A';
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+                result = 'B';
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+                result = 'C';
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+                result = 'D';
+            else
+                result = 'F';
+
+            return result;
         }
     }
 }
